c6301b408564523ebfa994128e668f82afcca342
shikamaru <rochelinshikamaru@gmail.com>
2022-08-14 14:33:10 +0200

    start Memory Game

---
+++ data.js
@@ -0,0 +1,131 @@
+export const data = [
+    {
+        name : "akatsuki",
+        img: "./images/akatsuki.jpeg"
+    },
+    {
+        name : "akatsuki",
+        img: "./images/akatsuki.jpeg"
+    },
+    {
+        name: "browser",
+        img: "./images/browser.png"
+    },
+    {
+        name: "browser",
+        img: "./images/browser.png"
+    },
+    {
+        name: "bulbizarre",
+        img: "./images/bulbizarre.jpeg"
+    },
+    {
+        name: "bulbizarre",
+        img: "./images/bulbizarre.jpeg"
+    },
+    {
+        name: "carapuce",
+        img: "./images/carapuce.jpeg"
+    },
+    {
+        name: "carapuce",
+        img: "./images/carapuce.jpeg"
+    },
+    {
+        name: "gojo",
+        img: "./images/gojo.jpeg"
+    },
+    {
+        name: "gojo",
+        img: "./images/gojo.jpeg"
+    },
+    {
+        name: "itadori",
+        img: "./images/itadori.jpeg"
+    },
+    {
+        name: "itadori",
+        img: "./images/itadori.jpeg"
+    },
+    {
+        name: "luigi",
+        img: "./images/luigi.png"
+    },
+    {
+        name: "luigi",
+        img: "./images/luigi.png"
+    },
+    {
+        name: "luigi1",
+        img: "./images/luigi1.jpeg"
+    },
+    {
+        name: "luigi1",
+        img: "./images/luigi1.jpeg"
+    },
+    {
+        name: "mario",
+        img: "./images/mario.jpeg"
+    },
+    {
+        name: "mario",
+        img: "./images/mario.jpeg"
+    },
+    {
+        name: "peach",
+        img: "./images/peach.jpeg"
+    },
+    {
+        name: "peach",
+        img: "./images/peach.jpeg"
+    },
+    {
+        name: "pikachu",
+        img: "./images/pikachu.jpeg"
+    },
+    {
+        name: "pikachu",
+        img: "./images/pikachu.jpeg"
+    },
+    {
+        name: "pikachuBoruto",
+        img: "./images/pikachuBoruto.jpeg"
+    },
+    {
+        name: "pikachuBoruto",
+        img: "./images/pikachuBoruto.jpeg"
+    },
+    {
+        name: "shikamaru",
+        img: "./images/shikamaru.jpeg"
+    },
+    {
+        name: "shikamaru",
+        img: "./images/shikamaru.jpeg"
+    },
+    {
+        name: "salameche",
+        img: "./images/salameche.jpeg"
+    },
+    {
+        name: "salameche",
+        img: "./images/salameche.jpeg"
+    },
+    {
+        name: "sukuna1",
+        img: "./images/sukuna1.jpeg"
+    },
+    {
+        name: "sukuna1",
+        img: "./images/sukuna1.jpeg"
+    },
+    {
+        name: "temari",
+        img: "./images/temari.png"
+    },
+    {
+        name: "temari",
+        img: "./images/temari.png"
+    }
+
+];
---
+++ index.js
@@ -0,0 +1,111 @@
+import {data} from "./data.js";
+
+const container = document.querySelector("#container");
+const h1 = document.querySelector("h1");
+const b = document.querySelector("b");
+const reload = document.querySelector(".reload");
+const button = document.querySelector("button");
+let vie = 10;
+let arr = [];
+let newArr = [];
+let score = 0;
+let bestScore ;
+
+// changement aleatoire de la position des objets dans le tableau
+data.sort(() => 0.5 - Math.random());
+
+// fonction permettant de creer des div et images dans le container
+function newDivImage (){
+    for(let i = 0; i < data.length; i++){
+        const card = document.createElement("div");
+        const image = document.createElement("img");
+         card.setAttribute("myId", i);
+         card.classList.add("backgroundCard");
+         image.setAttribute("myIdImage", `l${i}`);
+         image.setAttribute("src", data[i].img);
+         image.setAttribute("alt", data[i].name);
+         image.classList.add("image");
+         card.appendChild(image);  
+         container.appendChild(card);
+    }
+}
+newDivImage ();
+
+const card = document.querySelectorAll(".backgroundCard");
+
+
+    for (let i = 0; i < card.length; i++){
+        card[i].addEventListener("click", flipcard);
+     }
+    
+function flipcard (e){
+        let myId = parseInt(e.target.getAttribute("myId")); 
+        e.target.style.backgroundColor = "white";
+        document.querySelector(`[myIdImage = l${myId}]`).style.visibility = "visible";
+        arr.push(data[myId].name);
+        newArr.push(myId);
+        console.log(arr);
+      //  console.log(newArr);
+        console.log(container.children.length);
+        if (arr.length == 2 & newArr.length == 2){
+            if (arr[0] == arr[1]) {
+                score += 5;
+                h1.innerText = `Score: ${score}`;
+                setTimeout(()=>{
+                    removeCard(newArr[0], newArr[1]);
+                    newArr.map(item => card[item].style.visibility = "hidden");
+                    newArr = [];
+                }, 400);
+            }
+            else
+            {
+                vie--;
+                b.innerText = vie;
+                setTimeout(()=>{
+                removeCard(newArr[0], newArr[1]);
+                newArr.map(item => card[item].style.backgroundColor = "greenyellow");
+                newArr = [];
+                 },400);
+            }
+            arr = [];
+        }
+
+        if (vie == 0) {
+            bestScore = score;
+
+            for (let i = 0; i < card.length; i++){
+                card[i].removeEventListener("click", flipcard);
+             }
+            setTimeout(() =>{
+                reload.style.transform = "translate(180%, 80%)";
+                reload.style.transition = "transform ease-in-out 0.4s";
+                document.querySelector(".span").innerText = `Best Score: ${bestScore}`;
+            }, 400);
+        }
+        if (score == 80) {
+            setTimeout(() =>{
+                reload.style.transform = "translate(180%, 80%)";
+                reload.style.transition = "transform ease-in-out 0.4s";
+                document.querySelector(".span").innerText = `Congratulation`;
+            }, 400);
+        }
+       
+        // permet d'arreter l'evenement passer au parent des images
+        document.querySelector(`[myIdImage = l${myId}]`).onclick = (e)=>{
+                e.stopPropagation();
+        }
+    
+}
+
+
+function removeCard (item1, item2) {
+  document.querySelector(`[myIdImage = l${item1}]`).style.visibility = "hidden";
+  document.querySelector(`[myIdImage = l${item2}]`).style.visibility = "hidden";
+  
+}
+
+// button onclick to refresh a page
+
+button.onclick = () =>{
+    history.go(0);
+}
